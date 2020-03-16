35e8214f57344459a5cdd53d755d314689176cb6
starvii <starvii@gmail.com>
2020-03-16 15:45:38 +0800

    渗透题简化版完成

--- web/20200300_yingzhiyinghui/instagram-simple/script/deploy.sh
+++ web/20200300_yingzhiyinghui/instagram-simple/script/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -v
+# 注意，需要使用 bash deploy.sh 的方式运行，而不是 sh deploy.sh
 
-flag1="flag{c0931ce6-6ce8-4d55-ace3-33cb8b44e07c}"
-flag2="flag{1d910542-4588-4a6b-9ac2-54e13d39dd3d}"
-flag3="flag{5c377290-b08b-4d27-8a0b-ddbd80fcbb5b}"
+flag="flag{c0931ce6-6ce8-4d55-ace3-33cb8b44e07c}"
 
-prompt1="<%\n//你真棒！给你一个flag！\n//${flag1}\n//下一个flag在 /etc/passwd 中，想办法得到它吧~\n%>"
-prompt2="#${flag2}\n#下一个flag在/root/flag\n#知道什么是SUID吗？"
-prompt3="${flag3}"
+prompt="<%/* ${flag} */%>"
+
+robots="User-agent: *\nDisallow: /flag.jsp"
+
 
 webapps_path="/usr/share/tomcat/webapps"
 
@@ -30,26 +30,12 @@ firewall-cmd --zone=public --add-port=8009/tcp --permanent && \
 firewall-cmd --zone=public --remove-service=ssh --permanent && \
 firewall-cmd --reload && \
 
-# add war package
-tar zcf ${webapps_path}/ROOT.tgz ${webapps_path}/ROOT && \
-rm -rf ${webapps_path}/ROOT && \
-# 调用python直接解压
-python -c "import zipfile;z=zipfile.ZipFile('./instagram.war');[z.extract(f,'${webapps_path}/ROOT') for f in z.namelist()];z.close();" && \
-# chmod war package
-chown tomcat ${webapps_path}/ROOT/upload && \
-chmod 1644 ${webapps_path}/ROOT/upload/index.jsp && \
+# add robots.txt
+echo -e ${robots}>${webapps_path}/ROOT/robots.txt && \
 
-# attach to flag.jsp
-echo -e ${prompt1} >> ${webapps_path}/ROOT/flag.jsp && \
 
-# 修改/etc/passwd
-echo -e ${prompt2} >> /etc/passwd && \
-
-# 修改/usr/bin/tar属性
-chmod 4755 /usr/bin/tar && \
-
-# 添加/root/flag
-echo -e ${prompt3} > /root/flag && \
+# add flag.jsp
+echo -e ${prompt}>${webapps_path}/ROOT/flag.jsp && \
 
 # 清理痕迹
 rm -rf /root/.bash_history && \
